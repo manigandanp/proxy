1458f32b960fe460eab84141090bd6482f84b26a
Manigandan P <sabarinathan305@gmail.com>
2021-08-14 21:47:36 +0530

    return singelton instance of db

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,22 +2,30 @@
 
 import logger from "../../logger";
 import { Sequelize } from "sequelize";
-import Proxy from "../models/proxy.model";
+import config from "../../config";
+import path from "path";
+
+const DB_PATH = path.resolve(
+  __dirname,
+  `${config.db.path}/${config.db.fileName}`
+);
 
 const log = logger(module.filename);
 
-export class DB {
-  //Add singleton instance
+class DB {
   constructor(dbPath) {
+    console.log("instanation...");
     this.db = new Sequelize({
       dialect: "sqlite",
       storage: dbPath,
       logging: (msg) => log.info(msg),
     });
-
-    this.proxyModel = Proxy(this.db);
-    return { db: this.db, proxyModel: this.proxyModel };
+    return this.db;
   }
 }
+
+const db = new DB(DB_PATH);
+
+export { db };
 
 // https://www.bezkoder.com/node-js-express-sequelize-mysql/
