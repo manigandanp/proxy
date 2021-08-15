7e970ba28700f4cedead64be5daee701f0f275aa
Manigandan P <sabarinathan305@gmail.com>
2021-08-15 21:15:49 +0530

    update proxy routes

--- server/routes/proxy.routes.js
+++ server/routes/proxy.routes.js
@@ -1,14 +1,17 @@
 import express from "express";
+// import bodyParser from "body-parser";
 import logger from "../../logger";
+import { findAllProxies, createProxy } from "../controller/proxy.contoller";
 
 const router = express.Router();
 const log = logger(module.filename);
 
-const response = {};
-
-router.get("/", (req, res) => {
-  log.info("sending response");
-  res.json(response);
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+router.get("/", async (req, res) => {
+  log.info("sending all proxies response");
+  let proxies = await findAllProxies();
+  res.json(proxies);
 });
 
 module.exports = router;
