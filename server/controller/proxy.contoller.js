67a0b7513ea1514608f86b837e5d9f92547ba533
Manigandan P <sabarinathan305@gmail.com>
2021-09-25 12:52:06 +0530

    add bulk create endpoint

--- server/controller/proxy.contoller.js
+++ server/controller/proxy.contoller.js
@@ -1,7 +1,11 @@
 import { Proxy } from "../models/proxy.model";
 
 const createProxy = async (proxyObj) => await Proxy.create(proxyObj);
+
+const bulkCreateProxy = async (proxyObjArr) =>
+  await Proxy.bulkCreate(proxyObjArr);
+
 const findAllProxies = async (proxyObj) =>
   await Proxy.findAll({ attributes: ["ip", "port", "country", "protocol"] });
 
-export { createProxy, findAllProxies };
+export { createProxy, findAllProxies, bulkCreateProxy };
