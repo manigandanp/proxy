b8788518224794472f73cc26f4d5634ce5697ff2
Manigandan P <sabarinathan305@gmail.com>
2021-09-26 18:08:07 +0530

    add new parser for geonode proxy site

---
+++ src/fetcher/proxy-parser.js
@@ -0,0 +1,28 @@
+import cheerio from "cheerio";
+import { bulkCreateProxy } from "../server/controller/proxy.contoller";
+
+import { Crawler } from "./crawler";
+
+let url =
+  "https://proxylist.geonode.com/api/proxy-list?limit=100&page=1&sort_by=lastChecked&sort_type=desc&protocols=https";
+
+let crawler = new Crawler();
+
+console.log(url);
+
+crawler
+  .crawl(url)
+  .then((content) => {
+    let proxiesJson = content.data.data;
+    return proxiesJson.map(function (proxyObj) {
+      return {
+        host: proxyObj.ip,
+        port: proxyObj.port,
+        protocol: proxyObj.protocols[0],
+        country: proxyObj.country,
+      };
+    });
+  })
+  .then((proxies) => bulkCreateProxy(proxies))
+  .then((result) => console.log(result.data))
+  .catch((err) => console.log(err));
