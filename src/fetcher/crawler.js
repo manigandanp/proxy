15042a90a960c52c5d0fe681abb814b1c36478c1
Manigandan P <sabarinathan305@gmail.com>
2021-09-26 20:17:48 +0530

    remove crawl with proxy implementation

--- src/fetcher/crawler.js
+++ src/fetcher/crawler.js
@@ -20,23 +20,27 @@ export class Crawler {
     let userAgent = this.__helper.getRandomFromArr(config["userAgents"]);
     let crawlConfig = {
       proxy: proxy,
-      timeout: 10000,
+      timeout: 50000,
       headers: { "User-Agent": userAgent },
     };
     this.__log.info(
-      `Fetching ${url} with proxy config = ${proxy} and userAgent = ${userAgent} `
+      `Fetching ${url} with proxy config = ${
+        proxy ? proxy["host"] : proxy
+      } and userAgent = ${userAgent} `
     );
 
     return axios.get(url, crawlConfig);
   }
-
-  async crawl(url) {
-    let proxies = await findAllProxies().then((pr) =>
-      pr.map((p) => p.getDataValue())
-    );
-    let proxy = this.__helper.getRandomFromArr(proxies);
-
+  // Crawl using proxy implemention - implementaion is fine but proxies are not working
+  // async crawl(url) {
+  //   return await findAllProxies().then((pr) => {
+  //     let proxies = pr.map((p) => p.toJSON());
+  //     let proxy = this.__helper.getRandomFromArr(proxies);
+  //     return this.__crawlWithProxy(url, proxy);
+  //   });
+  // }
+  crawl(url) {
+    let proxy;
     return this.__crawlWithProxy(url, proxy);
   }
 }
-
