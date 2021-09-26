8510932049ea94b8bddfb258b6b6b8342b2ee3d4
Manigandan P <sabarinathan305@gmail.com>
2021-09-26 18:08:30 +0530

    update production logger settings

--- logger.js
+++ logger.js
@@ -6,23 +6,23 @@ const logFormat = printf(({ level, message, label, timestamp }) => {
 });
 
 const getTransports = (filename) => {
-  if (process.env.NODE_ENV !== "production") {
-    return [
-      new transports.Console({
-        // format: format.simple(),
-        format: combine(label({ label: filename }), timestamp(), logFormat),
-      }),
-    ];
-  } else {
-    //
-    // - Write to all logs with level `info` and below to `proxies-combined.log`
-    // - Write all logs error (and below) to `proxies-error.log`.
-    //
-    return [
-      new transports.File({ filename: "proxies-error.log", level: "error" }),
-      new transports.File({ filename: "proxies-combined.log" }),
-    ];
-  }
+  // if (process.env.NODE_ENV !== "production") {
+  return [
+    new transports.Console({
+      // format: format.simple(),
+      format: combine(label({ label: filename }), timestamp(), logFormat),
+    }),
+  ];
+  // } else {
+  //   //
+  //   // - Write to all logs with level `info` and below to `proxies-combined.log`
+  //   // - Write all logs error (and below) to `proxies-error.log`.
+  //   //
+  //   return [
+  //     new transports.File({ filename: "proxies-error.log", level: "error" }),
+  //     new transports.File({ filename: "proxies-combined.log" }),
+  //   ];
+  // }
 };
 
 const getFileName = (scriptPath) => scriptPath.split("/").slice(-1);
