e37078cefe53af08d75ba6db06e99f82cb210156
Manigandan P <sabarinathan305@gmail.com>
2021-09-27 21:03:42 +0530

    add delete logic if more than 1000 records

--- src/server/controller/proxy.contoller.js
+++ src/server/controller/proxy.contoller.js
@@ -1,11 +1,40 @@
 import { Proxy } from "../models/proxy.model";
+import { db as sequelize } from "../db/connect";
 
-const createProxy = (proxyObj) =>  Proxy.create(proxyObj);
+const { QueryTypes } = require("sequelize");
+
+const createProxy = (proxyObj) => Proxy.create(proxyObj);
 
 const bulkCreateProxy = async (proxyObjArr) =>
-  await Proxy.bulkCreate(proxyObjArr);
+  await deleteOldAndCreateNew().then((msg) => Proxy.bulkCreate(proxyObjArr), {
+    ignoreDuplicates: true,
+    updateOnDuplicate: ["createdAt", "updatedAt"],
+  });
 
-const findAllProxies = async () =>
-  await Proxy.findAll({ attributes: ["host", "port", "country", "protocol"] });
+const findAllProxies = async () => {
+  return await deleteOldAndCreateNew().then((msg) =>
+    Proxy.findAll({
+      attributes: [
+        "id",
+        "host",
+        "port",
+        "country",
+        "protocol",
+        "createdAt",
+        "hash",
+      ],
+    })
+  );
+};
+
+const deleteOldAndCreateNew = async () => {
+  return await sequelize.query(
+    "DELETE FROM `proxies` WHERE id IN (SELECT id  FROM `proxies` ORDER BY createdAt desc LIMIT -1 offset 1000)",
+    {
+      type: QueryTypes.DELETE,
+      model: Proxy,
+    }
+  );
+};
 
 export { createProxy, findAllProxies, bulkCreateProxy };
